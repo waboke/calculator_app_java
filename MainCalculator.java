7c6d8c17929605c44e0dcd90f89389f573039ee2
UmarIssa <157658060+UmarIssa@users.noreply.github.com>
2024-01-25 01:18:20 +0100

    implement basic methods for arithmetics

--- MainCalculator.java
+++ MainCalculator.java
@@ -1,5 +1,26 @@
-class Maincalculator{
-    public static void main(String[] args){
+import java.util.Scanner;
 
+class Maincalculator {
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+       
     }
 }
