7ce86a11a7402123079a0d23d869afbeac275e59
amirah-ay <aishahaske125@gmail.com>
2025-01-13 17:06:34 +0100

    Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,53 @@
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Basic Calculator!");
+        System.out.println("Choose an operation: ");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        System.out.print("Enter your choice (1-4): ");
+        int choice = scanner.nextInt();
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        double result;
+
+        switch (choice) {
+            case 1:
+                result = num1 + num2;
+                System.out.println("The result of addition is: " + result);
+                break;
+            case 2:
+                result = num1 - num2;
+                System.out.println("The result of subtraction is: " + result);
+                break;
+            case 3:
+                result = num1 * num2;
+                System.out.println("The result of multiplication is: " + result);
+                break;
+            case 4:
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("The result of division is: " + result);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Invalid choice. Please restart the program and choose a valid option.");
+        }
+
+        scanner.close();
+    }
+}
