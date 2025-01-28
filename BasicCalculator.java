43990e83799c6a7260d2775af9c68bb17cec8e8a
jiddatulkhaireey <hauwasaidushehu2005@gmail.com>
2025-01-28 12:11:09 -0800

    Add files via upload

---
+++ BasicCalculator.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /, %): ");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '%':
+                result = num1 % num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("Error: Division by zero");
+                    return;
+                }
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid operator");
+                return;
+        }
+
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+    }
+}
