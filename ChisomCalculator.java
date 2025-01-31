1b1064538672c7c04d834f65a9c137ae91f62472
Stephanie20300 <stephrich.igweh@gmail.com>
2025-01-31 16:53:26 +0100

    Update Stephanie calculator.java

--- ChisomCalculator.java
+++ ChisomCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ChisomCalculator {
+public class Stephanie Calculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
