a6bb3298408b22696f5fa46594860b83b5f859c6
sir-malikk <muawiyaabdulhaleem@gmail.com>
2025-02-02 21:23:56 +0100

    Create MuawiyaCalculator.java

---
+++ MuawiyaCalculator.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero!");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operator. Please use +, -, *, or /.");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+
+        scanner.close();
+    }
+}
