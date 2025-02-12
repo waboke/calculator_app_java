426f0da29e66c14d89c9c1ba623ce5c6bf888eff
sir-malikk <muawiyaabdulhaleem@gmail.com>
2025-02-12 14:38:59 +0100

    Update MuawiyaCalculator.java

--- MuawiyaCalculator.java
+++ MuawiyaCalculator.java
@@ -1,45 +1,175 @@
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SimpleCalculator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public class SwingCalculator extends JFrame implements ActionListener {
+    private JTextField display;
+    private JButton[] numberButtons = new JButton[10];
+    private JButton[] functionButtons = new JButton[8];
+    private JButton addButton, subButton, mulButton, divButton;
+    private JButton decButton, equButton, delButton, clrButton, sqrtButton, powButton;
+    private JPanel panel;
 
-        System.out.print("Enter the first number: ");
-        double num1 = scanner.nextDouble();
+    private double num1 = 0, num2 = 0, result = 0;
+    private char operator;
 
-        System.out.print("Enter the operator (+, -, *, /): ");
-        char operator = scanner.next().charAt(0);
+    public SwingCalculator() {
+        setTitle("Calculator");
+        setSize(420, 550);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
 
-        System.out.print("Enter the second number: ");
-        double num2 = scanner.nextDouble();
+        display = new JTextField();
+        display.setBounds(50, 25, 300, 50);
+        display.setEditable(false);
+        add(display);
 
-        double result;
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("Del");
+        clrButton = new JButton("Clr");
+        sqrtButton = new JButton("√");
+        powButton = new JButton("^");
 
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    System.out.println("Error: Division by zero!");
-                    return;
-                }
-                break;
-            default:
-                System.out.println("Invalid operator. Please use +, -, *, or /.");
-                return;
+        functionButtons[0] = addButton;
+        functionButtons[1] = subButton;
+        functionButtons[2] = mulButton;
+        functionButtons[3] = divButton;
+        functionButtons[4] = decButton;
+        functionButtons[5] = equButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = clrButton;
+
+        for (int i = 0; i < 8; i++) {
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(new Font("Arial", Font.BOLD, 20));
+            functionButtons[i].setFocusable(false);
         }
 
-        System.out.println("Result: " + result);
+        for (int i = 0; i < 10; i++) {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(new Font("Arial", Font.BOLD, 20));
+            numberButtons[i].setFocusable(false);
+        }
 
-        scanner.close();
+        sqrtButton.addActionListener(this);
+        powButton.addActionListener(this);
+        sqrtButton.setFont(new Font("Arial", Font.BOLD, 20));
+        powButton.setFont(new Font("Arial", Font.BOLD, 20));
+        sqrtButton.setFocusable(false);
+        powButton.setFocusable(false);
+
+        panel = new JPanel();
+        panel.setBounds(50, 100, 300, 300);
+        panel.setLayout(new GridLayout(5, 4, 10, 10));
+
+        panel.add(numberButtons[1]);
+        panel.add(numberButtons[2]);
+        panel.add(numberButtons[3]);
+        panel.add(addButton);
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(subButton);
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(mulButton);
+        panel.add(decButton);
+        panel.add(numberButtons[0]);
+        panel.add(equButton);
+        panel.add(divButton);
+        panel.add(sqrtButton);
+        panel.add(powButton);
+        panel.add(delButton);
+        panel.add(clrButton);
+
+        add(panel);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for (int i = 0; i < 10; i++) {
+            if (e.getSource() == numberButtons[i]) {
+                display.setText(display.getText().concat(String.valueOf(i)));
+            }
+        }
+        if (e.getSource() == decButton) {
+            display.setText(display.getText().concat("."));
+        }
+        if (e.getSource() == addButton) {
+            num1 = Double.parseDouble(display.getText());
+            operator = '+';
+            display.setText("");
+        }
+        if (e.getSource() == subButton) {
+            num1 = Double.parseDouble(display.getText());
+            operator = '-';
+            display.setText("");
+        }
+        if (e.getSource() == mulButton) {
+            num1 = Double.parseDouble(display.getText());
+            operator = '*';
+            display.setText("");
+        }
+        if (e.getSource() == divButton) {
+            num1 = Double.parseDouble(display.getText());
+            operator = '/';
+            display.setText("");
+        }
+        if (e.getSource() == sqrtButton) {
+            num1 = Double.parseDouble(display.getText());
+            result = Math.sqrt(num1);
+            display.setText(String.valueOf(result));
+        }
+        if (e.getSource() == powButton) {
+            num1 = Double.parseDouble(display.getText());
+            operator = '^';
+            display.setText("");
+        }
+        if (e.getSource() == equButton) {
+            num2 = Double.parseDouble(display.getText());
+
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                case '^':
+                    result = Math.pow(num1, num2);
+                    break;
+            }
+            display.setText(String.valueOf(result));
+            num1 = result;
+        }
+        if (e.getSource() == clrButton) {
+            display.setText("");
+        }
+        if (e.getSource() == delButton) {
+            String string = display.getText();
+            display.setText("");
+            for (int i = 0; i < string.length() - 1; i++) {
+                display.setText(display.getText() + string.charAt(i));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new SwingCalculator();
     }
 }
